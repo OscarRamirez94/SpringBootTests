ffb5c84f94af0df1a4eeff9bf8e13ee36b1129d3
OscarRamirez94 <al221310583@gmail.com>
2021-12-30 18:20:23 -0600

    pruebas de integracion con wc

---
+++ src/main/java/com/oscar/test/springboot/app/services/CuentaService.java
@@ -0,0 +1,24 @@
+package com.oscar.test.springboot.app.services;
+
+import com.oscar.test.springboot.app.models.Cuenta;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public interface CuentaService {
+
+    List<Cuenta> findAll();
+
+    Cuenta findById(Long id);
+
+    Cuenta save(Cuenta cuenta);
+
+    void deleteById(Long id);
+
+    int revisarTotalTransferencias(Long bancoId);
+
+    BigDecimal revisarSaldo(Long cuentaId);
+
+    void transferir(Long numCuentaOrigen, Long numCuentaDestino, BigDecimal monto,
+                    Long bancoId);
+}
