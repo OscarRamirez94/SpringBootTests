5808e4706c1c87460da740087808990cc96fb098
OscarRamirez94 <al221310583@gmail.com>
2021-12-30 23:19:56 -0600

    se integra jacoco y sonar

--- target/classes/import.sql
+++ target/classes/import.sql
@@ -1,3 +1,3 @@
-INSERT INTO cuentas (persona, saldo) VALUES ('AndrÃ©s', 1000);
+INSERT INTO cuentas (persona, saldo) VALUES ('Andrés', 1000);
 INSERT INTO cuentas (persona, saldo) VALUES ('John', 2000);
 INSERT INTO bancos (nombre, total_transferencias) VALUES ('El banco financiero', 0);
